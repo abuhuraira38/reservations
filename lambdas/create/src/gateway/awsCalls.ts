fb3fdf3da5ee92b4a8cbdc6ac134fd9892de9a85
abuhuraira38 <93987240+abuhuraira38@users.noreply.github.com>
2022-03-30 10:39:41 +0500

    Update awsCalls.ts

--- lambdas/create/src/gateway/awsCalls.ts
+++ lambdas/create/src/gateway/awsCalls.ts
@@ -2,7 +2,7 @@ import {DynamoDB} from "aws-sdk";
 import {DocumentClient} from "aws-sdk/lib/dynamodb/document_client";
 import PutItemInput = DocumentClient.PutItemInput;
 
-const client = new DynamoDB.DocumentClient({accessKeyId: 'AKIAUE4SGOF3ZUKTJ2GN', secretAccessKey: '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', region: "eu-west-1"});
+const client = new DynamoDB.DocumentClient({region: "eu-west-1"});
 
 export const save = (params: PutItemInput) => {
     return client.put(params).promise();
